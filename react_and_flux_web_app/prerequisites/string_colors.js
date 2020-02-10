46af19d1c466bcc6e5c9ff63353621b5bb2a2aef
choirul84 <aristhu_oracle@yahoo.com>
2020-02-10 14:12:02 +0700

    string color exercise => done

---
+++ react_and_flux_web_app/prerequisites/string_colors.js
@@ -0,0 +1,13 @@
+var colors = require('colors');
+
+console.log('First some yellow text'.yellow); 
+console.log('Underline that text'.underline.yellow);
+console.log('Make it bold and red'.bold.red);
+console.log('Double raindows all day long'.rainbow);
+console.log('Drop the bash'.trap);
+console.log('Drop the rainbow bash'.trap.rainbow);
+console.log('So are inverse style'.inverse);
+console.log('Zebras are so fun'.zebra);
+console.log('Background color attack'.bgRed);
+console.log('Use random styles'.random);
+console.log('America Yeah'.america);
